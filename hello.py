567a4d70f1a8c4e39e8306f2b65308fee100b3d4
AnishN <anish.narayanan32@gmail.com>
2015-04-18 03:15:04 -0400

    Attempting to setup google web fonts in heroku.

--- hello.py
+++ hello.py
@@ -1,16 +1,22 @@
 import os
 from flask import Flask
 from flask import render_template
+from flask import url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+	return render_template("index.html")
 
 @app.route('/hello')
 def hello():
-    return 'Hello World'
+    pass
+    #return 'Hello World'
+
+with app.test_request_context():
+	pass
+	#url_for('static', filename='main.css')
     
 if __name__ == '__main__':
     app.run()
