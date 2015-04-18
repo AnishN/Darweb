4c53dde9a6e5517f6524f738dec34a41f11767ca
AnishN <anish.narayanan32@gmail.com>
2015-04-18 02:06:42 -0400

    Testing render_template flask code.

--- hello.py
+++ hello.py
@@ -1,9 +1,16 @@
 import os
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/hello')
 def hello():
-	pass
-    #return 'Hello World!'
+    return 'Hello World'
+    
+if __name__ == '__main__':
+    app.run()
