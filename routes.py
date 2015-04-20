0ee75eb4fd3f5b7ba5b62b85d9ab9ebc5b62048c
AnishN <anish.narayanan32@gmail.com>
2015-04-20 02:48:01 -0400

    Created my first simuation - natural selection. Does not have instructions on the page yet.

--- routes.py
+++ routes.py
@@ -2,22 +2,13 @@ import os
 from flask import Flask
 from flask import render_template
 from flask import url_for
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return render_template("index.html")
-
-@app.route('/hello')
-def hello():
-    pass
-    #return 'Hello World'
-
-with app.test_request_context():
-	pass
-	#url_for('static', filename='main.css')
-	#url_for('static', filename='chartist.min.css')
     
 if __name__ == '__main__':
     app.run()
--- static/main.js
+++ static/main.js
@@ -6,7 +6,7 @@
  * Genetic drift and migration - bar graph
 */
 
-var LineGraph = function(data, containerId)
+var LineGraph = function(containerId)
 {
 	var graph = document.getElementById(containerId);
 	var style = window.getComputedStyle(graph, null);
@@ -26,8 +26,8 @@ var LineGraph = function(data, containerId)
 	this.xAxis = this.setXAxis()
 	this.yScale = this.setYScale()
 	this.yAxis = this.setYAxis()
-	
-	alert(this.xScale);
+	this.data = [];
+	this.lineColors = [];
 }
 
 LineGraph.prototype.setXScale = function()
@@ -130,16 +130,129 @@ LineGraph.prototype.drawYAxis = function()
         .text("Frequency (p)");
 }
 
+LineGraph.prototype.drawData = function()
+{
+	
+	if(this.data.length != 0)
+	{	
+		var xScale = this.xScale;
+		var yScale = this.yScale;
+		
+		
+		for(var i = 0; i < this.data.length; i++)
+		{
+			console.log(this.lineColors[i])
+			
+			//This is the accessor function we talked about above
+			var lineFunction = d3.svg.line()
+				.x(function(d) { return xScale(d.x); })
+				.y(function(d) { return yScale(d.y); })
+				.interpolate("linear");
+
+			//The line SVG Path we draw
+			var lineGraph = this.svg.append("path")
+				.attr("d", lineFunction(this.data[i]))
+				.attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")")
+				.attr("stroke", this.lineColors[i])
+				.attr("stroke-width", 1)
+				.attr("fill", "none")
+				.attr("shape-rendering", "crispEdges");
+		}
+				
+	}
+}
+
 LineGraph.prototype.draw = function()
 {
+	d3.selectAll("svg > *").remove();//clears the entirety of d3
 	this.drawTitle()
 	this.drawXAxis()
 	this.drawYAxis()
 	this.drawGrid()
+	this.drawData()
+}
+
+LineGraph.prototype.addLineData = function(data)
+{
+	this.data.push(data)
+	var color = Math.floor(Math.random() * 16777216).toString(16);
+	var hexColor = '#000000'.slice(0, -color.length) + color;
+	this.lineColors.push(hexColor);
 }
 
 var main = function()
 {					
-	var graph = new LineGraph([], "graph")
-	graph.draw()
+	var form = document.getElementById("form1");
+	form.elements["p"].value = 0.5
+	form.elements["w11"].value = 0.5
+	form.elements["w12"].value = 0.5
+	form.elements["w22"].value = 0.5
+	
+	window.graph = new LineGraph("graph")
+	window.graph.draw()
+}
+
+var submitData = function()
+{
+	var form = document.getElementById("form1");
+	var p = parseFloat(form.elements["p"].value);
+	var w11 = parseFloat(form.elements["w11"].value);
+	var w12 = parseFloat(form.elements["w12"].value);
+	var w22 = parseFloat(form.elements["w22"].value);
+	
+	var pCheck = (p >= 0.0 && p <= 1.0);
+	var w11Check = (w11 >= 0.0 && w11 <= 1.0);
+	var w12Check = (w12 >= 0.0 && w12 <= 1.0);
+	var w22Check = (w22 >= 0.0 && w22 <= 1.0);
+	
+	if(! pCheck) form.elements["p"].style.color = "red";
+	else form.elements["p"].style.color = "8c8c8c";
+	if(! w11Check) form.elements["w11"].style.color = "red";
+	else form.elements["w11"].style.color = "8c8c8c";
+	if(! w12Check) form.elements["w12"].style.color = "red";
+	else form.elements["w12"].style.color = "8c8c8c";
+	if(! w22Check) form.elements["w22"].style.color = "red";
+	else form.elements["w22"].style.color = "8c8c8c";
+	
+	if(pCheck && w11Check && w12Check && w22Check)
+	{
+		data = runSimuation(p, w11, w12, w22)
+		window.graph.addLineData(data)
+		window.graph.draw()
+	}
+}
+
+var clearData = function()
+{
+	window.graph.data = [];
+	window.graph.lineColors = [];
+	window.graph.draw()
+}
+
+/*Does the actual evolutionary genetics work!*/
+var runSimuation = function(p, fit11, fit12, fit22)
+{
+	var numGen = 100
+	var maxAbsFit = Math.max(fit11, fit12, fit22)
+	var w11 = fit11 / maxAbsFit
+	var w12 = fit12 / maxAbsFit
+	var w22 = fit22 / maxAbsFit
+	var data = [{x: 0, y: p}]
+	
+	for(var i = 1; i < numGen + 1; i++)
+	{
+		var p2 = p * p;
+		var pq2 = 2 * p * (1 - p)
+		var q2 = (1 - p) * (1 - p)
+		var wBar = p2 * w11 + pq2 * w12 + q2 * w22
+		
+		var normp2 = p2 * w11 / wBar
+		var norm2pq = pq2 * w12 / wBar
+		var normq2 = q2 * w22 / wBar
+		var newP = normp2 + norm2pq / 2.0
+		data.push({x: i, y: newP})
+		p = newP
+	}
+	
+	return data
 }
